ece85770ba3db436d6dad5e4e4bdd2338739ac8c
Dery Jon <yusupoviskandar1005@gmail.com>
2024-08-13 19:12:29 +0500

    feat: add tailwind and shadcn (2024-08-13, 19:12)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"],
+
+  compatibilityDate: "2024-13-09",
+});
